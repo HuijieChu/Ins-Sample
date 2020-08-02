9e710b3ec2b5b6074f864a8910bc11e3a2f75b12
Haley <chuhuijie@hotmail.com>
2020-08-01 20:36:52 -0400

    add user

--- Insta/urls.py
+++ Insta/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from Insta.views import HelloWorld
+from Insta.views import HelloWorld, PostView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 urlpatterns = [
 path('', HelloWorld.as_view(), name='helloworld'),
+path('posts/', PostView.as_view(), name='posts'),
+path('post/<int:pk>/', PostDetailView.as_view(), name ='post_detail'),
+path('posts/new/',PostCreateView.as_view(), name = 'make_post'),
+path('post/update/<int:pk>/', PostUpdateView.as_view(), name ='post_update'),
+path('post/delete/<int:pk>/', PostDeleteView.as_view(), name ='post_delete'),
 ]
--- Insta/views.py
+++ Insta/views.py
@@ -1,4 +1,39 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView,DeleteView,UpdateView
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from Insta.models import Post
+from Insta.forms import CustomerUserCreationForm
 # Create your views here.
 class HelloWorld(TemplateView):
     template_name = 'test.html'
+
+class PostView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = '__all__'
+    login_url = 'login'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = ['title']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('posts')
+
+class SignUp(CreateView):
+    form_class = CustomerUserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy('login')
